fd7a044afd6f76fe1ea6ab6dee8c6c80bf67ecf0
tomebore <akylaimanalieva@gmail.com>
2020-09-23 22:37:31 +0600

    added Publication model

--- publication/admin.py
+++ publication/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+@admin.register(Publication)
+class PublicationAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "image",
+        "description",
+        "user",
+        "created",
+        "updated",
+        "deleted"
+    ]
+    
+    fields = [
+        "image",
+        "description",
+        "user"
+    ]
--- publication/models.py
+++ publication/models.py
@@ -1,7 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+from core.models import BaseModel
 
-# class Publication():
-#     user =
-#     description = 
-#     image = 
+class Publication(BaseModel):
+    user = models.ForeignKey(
+        to=User,
+        on_delete=models.CASCADE,
+        related_name="publication",
+        verbose_name="Владелец"
+    )
+    description = models.TextField(
+        null=True,blank=True,
+        verbose_name="Описание"
+    )
+    image = models.ImageField(
+        upload_to="publications",
+        verbose_name="Картинка"
+    )
 
